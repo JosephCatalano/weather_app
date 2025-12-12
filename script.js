6de3b6d159ca4d4ec5e6b2a2f7f332943837b75e
Joseph Catalano <joseph1catalano@gmail.com>
2025-12-12 00:19:22 -0500

    Initial Commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const CITY = document.getElementById("city");
+const SEARCH = document.getElementById("search");
+const DISPLAY_AREA = document.getElementById("weather");
+
+function getCity() {}
+
+SEARCH.addEventListener();
