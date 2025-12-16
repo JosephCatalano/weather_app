6bbe40ee36f67331a5e6f70c6338dc9674fb2636
Joseph <joseph1catalano@gmail.com>
2025-12-16 16:58:35 -0500

    Got API fetch working: App is able to make a request from api and display basic data on the ui

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
-const SEARCH = document.getElementById("search");
+import { WEATHER_API_KEY } from "./config.js";
+
+const SEARCHBUTTON = document.getElementById("search");
 const DISPLAY_AREA = document.getElementById("weather");
 const INPUTBOX = document.getElementById("inputBox");
+const TEMP = document.getElementById("temp");
+const DESCRIPTION = document.getElementById("desc");
+const HUMIDITY = document.getElementById("humidity");
+const WINDSPEED = document.getElementById("wind");
+const CITY = document.getElementById("city");
 
-function fetchWeatherData(city) {
-  null;
+async function fetchWeatherData(city) {
+  const response = await fetch(
+    `http://api.weatherapi.com/v1/current.json?key=${WEATHER_API_KEY}&q=${city}`
+  );
+  const data = await response.json();
+  return data;
 }
 
-function handleSearch() {
+async function handleSearch() {
   const userInput = INPUTBOX.value;
-  console.log(userInput);
-  fetchWeatherData();
+  const data = await fetchWeatherData(userInput);
+  console.log(data);
+
+  renderWeather(data);
 }
 
-SEARCH.addEventListener("click", handleSearch);
+function renderWeather(weather) {
+  CITY.textContent = `${weather.location.name}`;
+  TEMP.textContent = `${weather.current.temp_c} C`;
+  HUMIDITY.textContent = `${weather.current.humidity}%`;
+  WINDSPEED.textContent = `${weather.current.gust_kph} kph`;
+}
+
+SEARCHBUTTON.addEventListener("click", handleSearch);
 
 function buildUrl(city) {
   null;
